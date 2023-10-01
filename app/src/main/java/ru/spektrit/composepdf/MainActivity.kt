ac83b5580a2df00ea5563c4dbedc1cbfbe35c0ed
arcanegolem <dopeflipper@gmail.com>
2023-10-01 18:16:30 +0300

    Refactor

--- app/src/main/java/ru/spektrit/composepdf/MainActivity.kt
+++ app/src/main/java/ru/spektrit/composepdf/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import ru.spektrit.composepdf.ui.theme.ComposePDFTheme
-import ru.spektrit.pdfcompose.viewdocument.PdfViewer
 
 class MainActivity : ComponentActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +17,7 @@ class MainActivity : ComponentActivity() {
             Surface(
                modifier = Modifier.fillMaxSize(),
                color = MaterialTheme.colorScheme.background
-            ) {
-               PdfViewer(
-                  url = "http://tndt.idspektr.ru/images/stories/archive/03_2023/tndt_2023_03.pdf",
-                  headers = hashMapOf( "headerKey" to "headerValue" )
-               )
-            }
+            ) {  }
          }
       }
    }
