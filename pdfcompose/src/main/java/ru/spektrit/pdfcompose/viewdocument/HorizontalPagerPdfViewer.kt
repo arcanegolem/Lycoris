744e7c89f7581b047e36046c699a6241b9320719
arcanegolem <dopeflipper@gmail.com>
2023-10-15 22:13:31 +0300

    Added url and uri support for HorizontalPagerPdfViewer

--- pdfcompose/src/main/java/ru/spektrit/pdfcompose/viewdocument/HorizontalPagerPdfViewer.kt
+++ pdfcompose/src/main/java/ru/spektrit/pdfcompose/viewdocument/HorizontalPagerPdfViewer.kt
@@ -2,6 +2,8 @@ package ru.spektrit.pdfcompose.viewdocument
 
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
+import android.net.Uri
+import android.os.ParcelFileDescriptor
 import android.util.Log
 import androidx.annotation.RawRes
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -32,7 +34,11 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import retrofit2.http.Url
+import ru.spektrit.pdfcompose.network.provideDownloadInterface
 import ru.spektrit.pdfcompose.utils.PfdHelper
+import ru.spektrit.pdfcompose.utils.provideFileName
+import java.io.File
 import kotlin.math.sqrt
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -80,6 +86,189 @@ fun HorizontalPagerPdfViewer(
                      val destinationBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
                      mutex.withLock {
                         Log.d("PdfGenerator", "Loading PDF $documentDescription - page ${index + 1}/${r.pageCount}")
+
+                        if (!coroutineContext.isActive) return@launch
+
+                        try {
+                           r.let{
+                              it.openPage(index).use { rPage ->
+                                 rPage.render(destinationBitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+                              }
+                           }
+                        } catch (e: Exception) { return@launch }
+                     }
+                     bitmap = destinationBitmap
+                  }
+                  onDispose { job.cancel() }
+               }
+               PagePlaceHolder(backgroundColor = Color.White)
+            } else {
+               val request = ImageRequest.Builder(context)
+                  .size(width, height)
+                  .memoryCacheKey(cacheKey)
+                  .data(bitmap)
+                  .build()
+
+               Box {
+                  Image(
+                     modifier = Modifier
+                        .aspectRatio(1f / sqrt(2f))
+                        .fillMaxWidth(),
+                     painter = rememberAsyncImagePainter(request),
+                     contentDescription = "Page ${index + 1} of ${r.pageCount}"
+                  )
+               }
+            }
+         }
+      }
+   }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HorizontalPagerPdfViewer(
+   modifier: Modifier = Modifier.fillMaxWidth(),
+   uri: Uri,
+) {
+   val rendererScope = rememberCoroutineScope()
+   val mutex = remember { Mutex() }
+
+   val context = LocalContext.current
+
+   val renderer by produceState<PdfRenderer?>(null, null) {
+      rendererScope.launch(Dispatchers.IO) {
+         val pfdHelper = PfdHelper()
+         value = PdfRenderer(pfdHelper.getPfd(uri))
+      }
+      awaitDispose {
+         val currentRenderer = value
+         rendererScope.launch(Dispatchers.IO) {
+            mutex.withLock { currentRenderer?.close() }
+         }
+      }
+   }
+
+   renderer?.let { r ->
+      BoxWithConstraints(modifier = modifier) {
+         val width = with(LocalDensity.current) { maxWidth.toPx() }.toInt()
+         val height = (width * sqrt(2f)).toInt()
+
+         val imageLoadingScope = rememberCoroutineScope()
+         val imageLoader = context.imageLoader
+
+         HorizontalPager(pageCount = r.pageCount) { index ->
+            val cacheKey = MemoryCache.Key("$uri - $index")
+            val cacheValue : Bitmap? = imageLoader.memoryCache?.get(cacheKey)?.bitmap
+
+            var bitmap : Bitmap? by remember { mutableStateOf(cacheValue) }
+
+            if (bitmap == null) {
+               DisposableEffect(uri, index) {
+                  val job = imageLoadingScope.launch(Dispatchers.IO) {
+                     val destinationBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                     mutex.withLock {
+                        Log.d("PdfGenerator", "Loading PDF $uri - page ${index + 1}/${r.pageCount}")
+
+                        if (!coroutineContext.isActive) return@launch
+
+                        try {
+                           r.let{
+                              it.openPage(index).use { rPage ->
+                                 rPage.render(destinationBitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+                              }
+                           }
+                        } catch (e: Exception) { return@launch }
+                     }
+                     bitmap = destinationBitmap
+                  }
+                  onDispose { job.cancel() }
+               }
+               PagePlaceHolder(backgroundColor = Color.White)
+            } else {
+               val request = ImageRequest.Builder(context)
+                  .size(width, height)
+                  .memoryCacheKey(cacheKey)
+                  .data(bitmap)
+                  .build()
+
+               Box {
+                  Image(
+                     modifier = Modifier
+                        .aspectRatio(1f / sqrt(2f))
+                        .fillMaxWidth(),
+                     painter = rememberAsyncImagePainter(request),
+                     contentDescription = "Page ${index + 1} of ${r.pageCount}"
+                  )
+               }
+            }
+         }
+      }
+   }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HorizontalPagerPdfViewer(
+   modifier: Modifier = Modifier.fillMaxWidth(),
+   @Url url: String,
+   headers: HashMap<String, String>,
+) {
+   val rendererScope = rememberCoroutineScope()
+   val mutex = remember { Mutex() }
+
+   val bufferSize = 8192
+
+   val context = LocalContext.current
+
+   val renderer by produceState<PdfRenderer?>(null, null) {
+      rendererScope.launch(Dispatchers.IO) {
+         val file = File(context.cacheDir, provideFileName())
+         val response = provideDownloadInterface(headers).downloadFile(url)
+         val responseByteStream = response.byteStream()
+         context.filesDir
+
+         responseByteStream.use { inputStream ->
+            file.outputStream().use { outputStream ->
+               var data = ByteArray(bufferSize)
+               var count = inputStream.read(data)
+               while (count != -1) {
+                  outputStream.write(data, 0, count)
+                  data = ByteArray(bufferSize)
+                  count = inputStream.read(data)
+               }
+            }
+         }
+
+         value = PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY))
+      }
+      awaitDispose {
+         val currentRenderer = value
+         rendererScope.launch(Dispatchers.IO) {
+            mutex.withLock { currentRenderer?.close() }
+         }
+      }
+   }
+
+   renderer?.let { r ->
+      BoxWithConstraints(modifier = modifier) {
+         val width = with(LocalDensity.current) { maxWidth.toPx() }.toInt()
+         val height = (width * sqrt(2f)).toInt()
+
+         val imageLoadingScope = rememberCoroutineScope()
+         val imageLoader = context.imageLoader
+
+         HorizontalPager(pageCount = r.pageCount) { index ->
+            val cacheKey = MemoryCache.Key("$url - $index")
+            val cacheValue : Bitmap? = imageLoader.memoryCache?.get(cacheKey)?.bitmap
+
+            var bitmap : Bitmap? by remember { mutableStateOf(cacheValue) }
+
+            if (bitmap == null) {
+               DisposableEffect(url, index) {
+                  val job = imageLoadingScope.launch(Dispatchers.IO) {
+                     val destinationBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                     mutex.withLock {
+                        Log.d("PdfGenerator", "Loading PDF $url - page ${index + 1}/${r.pageCount}")
 
                         if (!coroutineContext.isActive) return@launch
 
