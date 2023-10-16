a5d1b50ef80a93bddc47355fb8dd8ae84a3c7c82
arcanegolem <dopeflipper@gmail.com>
2023-10-16 23:18:45 +0300

    Dependency update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
    implementation("androidx.core:core-ktx:1.12.0")
    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-   implementation("androidx.activity:activity-compose:1.7.2")
+   implementation("androidx.activity:activity-compose:1.8.0")
    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
    implementation("androidx.compose.ui:ui")
    implementation("androidx.compose.ui:ui-graphics")
@@ -66,5 +66,6 @@ dependencies {
    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
    debugImplementation("androidx.compose.ui:ui-tooling")
    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
    implementation(project(":lycoris"))
 }
