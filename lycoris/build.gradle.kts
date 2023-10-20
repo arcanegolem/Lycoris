a1fbb22c3a647b99bc18fcec4ee05a2f5d828881
Mikołaj Pich <m.pich@outlook.com>
2023-10-20 14:00:09 +0200

    Add maven publish config

--- lycoris/build.gradle.kts
+++ lycoris/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
    id("com.android.library")
    id("org.jetbrains.kotlin.android")
+   id("maven-publish")
 }
 
 android {
@@ -39,6 +40,11 @@ android {
    kotlinOptions {
       jvmTarget = "1.8"
    }
+   publishing {
+      singleVariant("release") {
+         withSourcesJar()
+      }
+   }
 }
 
 dependencies {
@@ -61,4 +67,18 @@ dependencies {
    // Retrofit 2
    implementation("com.squareup.retrofit2:retrofit:2.9.0")
    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+}
+
+publishing {
+   publications {
+      register<MavenPublication>("release") {
+         groupId = "ru.spektrit"
+         artifactId = "pdfcompose"
+         version = "0.1.0"
+
+         afterEvaluate {
+            from(components["release"])
+         }
+      }
+   }
 }
